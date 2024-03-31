5b6d1420ccab7f753f788dba38cb195fdcfeb76f
Jailsonr12 <jailson.roth@outlook.com>
2024-03-31 17:21:19 -0300

    adicionando formatação

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-
 function Home() {
-    return (
-        <div>
-        <h1>To com sdd, queria vc comigo s2</h1> 
-        <h3>by Jailson Roth</h3> 
-        </div>
-    );
+  return (
+    <div>
+      <h1>To com sdd, queria vc comigo s2</h1>
+      <h3>by Jailson Roth</h3>
+    </div>
+  );
 }
 
 export default Home;
