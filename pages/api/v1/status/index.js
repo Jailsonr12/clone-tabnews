e696c4b146accacdd643e3ceb9bd54f912bccd17
Jailsonr12 <jailson.roth@outlook.com>
2024-06-23 19:34:43 -0300

    Finalish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Vocẽ é um aluno acima da media" });
-}
+  const updatedAt = new Date().toISOString();
 
+  const databaseVerionResult = await database.query("SHOW server_version");
+  const databaseVersionvalue = databaseVerionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionResultValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsResultValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionvalue,
+        max_connections: parseInt(databaseMaxConnectionResultValue),
+        opened_connections: databaseOpenedConnectionsResultValue,
+      },
+    },
+  });
+}
 export default status;
